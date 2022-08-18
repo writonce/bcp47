6f42f7190443c3151020b62694aca071a735fd4c
jerCarre <22233742+jerCarre@users.noreply.github.com>
2022-08-18 13:11:17 +0200

    wip

--- bcp47_test.go
+++ bcp47_test.go
@@ -15,17 +15,14 @@ func TestCheckBCP47(T *testing.T) {
         ExpectedExit   int
         ExpectedOutput string
     }{
-        {"ok1", []string{"fr-fr"}, 0, "fr-FR\n"},
-        {"ok2", []string{"fr-FR"}, 0, "fr-FR\n"},
-        {"ok3", []string{"FR-FR"}, 0, "fr-FR\n"},
-        {"ko1", []string{""}, 1, "Missing language code\n"},
+        {"ok1", "fr-fr", 0, "fr-FR"},
+        {"ok2", "fr-FR", 0, "fr-FR"},
+        {"ok3", "FR-FR", 0, "fr-FR"},
+        {"ko1", "", 1, "Missing language code\n"},
     }
     for _, tc := range cases {
-        // this call is required because otherwise flags panics,
-        // if args are set between flag.Parse call
         flag.CommandLine = flag.NewFlagSet(tc.Name, flag.ExitOnError)
-        // we need a value to set Args[0] to cause flag begins parsing at Args[1]
-        os.Args = append([]string{tc.Name}, tc.Args...)
+        os.Args = append([]string{tc.Args})
         var buf bytes.Buffer
         actualExit := checkBCP47(&buf)
         if tc.ExpectedExit != actualExit {
